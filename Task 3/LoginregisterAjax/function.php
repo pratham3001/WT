3b62ae1ffa34d6bb65f557b898a47cf28855ef19
prajyot11 <76609343+prajyot11@users.noreply.github.com>
2022-05-02 22:37:27 +0530

    123

---
+++ Task 3/LoginregisterAjax/function.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASSWORD", "Pratik@1607");
+    define("DB_DATABASE", "loginregister");
+
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+// IF
+if(isset($_POST["action"])){
+  if($_POST["action"] == "register"){
+    register();
+  }
+  else if($_POST["action"] == "login"){
+    login();
+  }
+}
+
+// REGISTER
+function register(){
+  global $conn;
+
+  $name = $_POST["name"];
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  if(empty($name) || empty($username) || empty($password)){
+    echo "Please Fill Out The Form!";
+    exit;
+  }
+
+  $user = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username'");
+  if(mysqli_num_rows($user) > 0){
+    echo "Username Has Already Taken";
+    exit;
+  }
+
+  $query = "INSERT INTO tb_user VALUES('', '$name', '$username', '$password')";
+  mysqli_query($conn, $query);
+  echo "Registration Successful";
+}
+
+// LOGIN
+function login(){
+  global $conn;
+
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  $user = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username'");
+
+  if(mysqli_num_rows($user) > 0){
+
+    $row = mysqli_fetch_assoc($user);
+
+    if($password == $row['password']){
+      echo "Login Successful";
+      $_SESSION["login"] = true;
+      $_SESSION["id"] = $row["id"];
+    }
+    else{
+      echo "Wrong Password";
+      exit;
+    }
+  }
+  else{
+    echo "User Not Registered";
+    exit;
+  }
+}
+?>
